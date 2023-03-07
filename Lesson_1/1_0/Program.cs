325f95a9a03bc6c31f02b916530f7351a6570951
Ivan <vanuk@bk.ru>
2023-03-07 15:45:48 +0800

    Task 1_0

---
+++ Lesson_1/1_0/Program.cs
@@ -0,0 +1,7 @@
+﻿int num = 0;
+
+Console.Write("Please enter number n = ");
+// num = Convert.ToInt32(Console.ReadLine()); // мой вариант
+num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("n^2 = " + (num * num));
