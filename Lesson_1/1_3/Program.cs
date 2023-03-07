246ca978f4f343ccffbcfd7c5efa5f1645a6aa52
Ivan <vanuk@bk.ru>
2023-03-07 16:22:53 +0800

    Task 1_3

---
+++ Lesson_1/1_3/Program.cs
@@ -0,0 +1,25 @@
+﻿int num = 0;
+
+Console.Write("Please enter positive integer n = ");
+num = int.Parse(Console.ReadLine()!);
+
+if (num < 0)
+{
+    for (int i = -num; i >= num; i--)
+    {
+        Console.Write(i + " ");
+    }
+}
+else if (num > 0)
+{
+    for (int i = -num; i <= num; i++)
+    {
+        Console.Write(i + " ");
+    }
+}
+else
+{
+    Console.Write("0");
+}
+
+
