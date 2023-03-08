f7aeab196de6d8f1960652874b11561fe5f8e34f
Ivan <vanuk@bk.ru>
2023-03-08 11:24:43 +0800

    homework 1_2

---
+++ Lesson_1/HW_1_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int[] numbers = {0,0,0};
+int length = numbers.Length;
+
+for (int i = 0; i < length; i++)
+{
+    Console.Write($"Please enter number{i+1} n{i+1} = ");
+    numbers[i] = int.Parse(Console.ReadLine()!);
+}
+
+int max = numbers[0];
+
+for (int j = 1; j < length; j++)
+{
+    if (numbers[j] > max)
+    {
+        max = numbers[j];
+    }
+}
+
+Console.WriteLine("max = " + max);
