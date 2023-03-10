dbb843c8ddeecb7d2cafec9fe7c7dc5e4a99fc3b
Ivan <vanuk@bk.ru>
2023-03-10 15:29:17 +0800

    task 2_0

---
+++ Lesson_2/2_0/Program.cs
@@ -0,0 +1,22 @@
+﻿int num = GenerateNumber();
+PrintLastDigit(num);
+
+
+int GetNumber()
+{
+    Console.Write("Please enter number n = ");
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int GenerateNumber()
+{
+    int number = new Random().Next(100, 1000);
+    return number;
+}
+
+void PrintLastDigit (int n)
+{
+    Console.WriteLine($"Last digit of number {n} is {n%10}");
+}
+
