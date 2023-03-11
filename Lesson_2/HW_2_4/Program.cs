5d077a9f821b9560d2b7ba067b38875f4597aa8a
Ivan <vanuk@bk.ru>
2023-03-11 21:06:01 +0800

    homework_2_4

---
+++ Lesson_2/HW_2_4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа
+// На сайте этого задания не было, но в методичке есть, на всякий случай выполнил
+
+int num = GenerateNumber();
+PrintDoubleDigitNumber (num, CreateDoubleDigitNumber(GetFirstDigit(num), GetLastDigit(num)));
+
+int GenerateNumber()
+{
+    int number = new Random().Next(100, 1000);
+    return number;
+}
+
+int GetFirstDigit(int n)
+{
+    return n / 100;
+}
+
+int GetLastDigit(int n)
+{
+    return n % 10;
+}
+
+int CreateDoubleDigitNumber(int numFirst, int numLast)
+{
+    return numFirst * 10 + numLast;
+}
+
+void PrintDoubleDigitNumber (int initNum, int doubleDigNum)
+{
+    Console.WriteLine($"{initNum} -> {doubleDigNum}");
+}
