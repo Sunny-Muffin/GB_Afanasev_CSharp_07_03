77a193d6b40285e64ce00bea6fadde0b0e6c224f
Ivan <vanuk@bk.ru>
2023-03-14 16:07:24 +0800

    3_2

---
+++ Lesson_3/3_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Please enter 4 coordinates in a row: ");
+Console.WriteLine(FindDistance(GetCoordinate(), GetCoordinate(), GetCoordinate(), GetCoordinate()));
+
+
+int GetCoordinate()
+{
+    int c = int.Parse(Console.ReadLine()!);
+    return c;
+}
+
+double FindDistance(int x1, int y1, int x2, int y2)
+{
+    return Math.Round(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)), 2);
+}
