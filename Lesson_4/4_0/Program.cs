dee880a29447c5b610fb5c98b8817a194176e67e
Ivan <vanuk@bk.ru>
2023-03-17 15:22:30 +0800

    4_0

--- Lesson_4/4_0/Program.cs
+++ Lesson_4/4_0/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+int num = GetUserNumber();
+Console.WriteLine($"{num} -> {SumNumbers(num)}");
+
+int GetUserNumber()
+{
+    int number = 0;
+    while(number == 0)
+    {
+        Console.Write("Please enter number: ");
+        number = int.Parse(Console.ReadLine()!);
+    }
+    return number;
+}
+
+int SumNumbers(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; ++i)
+    {
+        sum += i;
+    }
+    return sum;
+}
