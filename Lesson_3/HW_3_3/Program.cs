e05890599c368a833e72037a8827d4a3bfb56709
Ivan <vanuk@bk.ru>
2023-03-17 12:49:27 +0800

    homework 3_3

---
+++ Lesson_3/HW_3_3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+PrintCubesOfNum(GetUserNumber());
+
+int GetUserNumber()
+{
+    int number = 0;
+    while(number == 0)
+    {
+        Console.Write("Please enter number: ");
+        number = int.Parse(Console.ReadLine()!);
+    }
+    return number;
+}
+
+void PrintCubesOfNum(int number)
+{
+    if(number > 0)
+    {
+        Console.Write(number + " -> 1");
+        for (int i = 2; i <= number; ++i)
+        {
+            Console.Write(", " + i*i*i);
+        }
+    }
+    else
+    {
+        Console.Write(number + " -> -1");
+        for (int i = -2; i >= number; --i)
+        {
+            Console.Write(", " + i*i*i);
+        }
+    }
+    Console.WriteLine();
+}
+
