9bd78a7ee743dc03548a7e41b0d2df1ee10495d9
Ivan <vanuk@bk.ru>
2023-03-18 12:29:30 +0800

    homework_4_1

---
+++ Lesson_4/HW_4_1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+
+int num = GetUserNumber("number");
+int pow = GetUserNumber("power");
+
+Console.WriteLine($"{num}^{pow} = {ToThePower(num, pow)}");
+
+int GetUserNumber(string name)
+{
+    int number = 0;
+    Console.Write($"Please enter {name}: ");
+    number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+double ToThePower(int n, int p)
+{
+    double newNum = 1;
+    if(p > 0)
+    {
+        for (int i = 0; i < p; ++i)
+        {
+            newNum *= n;
+        }
+    }
+    else if (p < 0)
+    {
+        for (int i = 0; i > p; --i)
+        {
+            newNum /= n;
+        }
+    }
+    return Math.Round(newNum, 4);
+}
+
