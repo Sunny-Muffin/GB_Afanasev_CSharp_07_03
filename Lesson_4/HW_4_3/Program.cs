f8041731c2c6ce472f38b9c9265afcf2c9bd1428
Ivan <vanuk@bk.ru>
2023-03-18 12:49:09 +0800

    homework_4_3

---
+++ Lesson_4/HW_4_3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+// Переделал задачу под N элементов, так универсальнее и интереснее
+
+int arrLength = GetUserNumber("array length");
+int arrMin = GetUserNumber("min element");
+int arrMax = GetUserNumber("max element");
+
+PrintArray (MakeArray(arrLength, arrMin, arrMax));
+
+int GetUserNumber(string name)
+{
+    int number = 0;
+    Console.Write($"Please enter {name}: ");
+    number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int[] MakeArray(int length, int min, int max)
+{
+    int[] arr = new int[length];
+    for(int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    int len = array.Length;
+    for (int i = 0; i < len; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
