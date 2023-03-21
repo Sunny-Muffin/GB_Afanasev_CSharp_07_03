f8fa433a3b8ab8cb23653f9e82a91a8579542d86
Ivan <vanuk@bk.ru>
2023-03-21 15:27:05 +0800

    5_0

--- Lesson_5/5_0/Program.cs
+++ Lesson_5/5_0/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+
+int[] arr = MakeArray(12, -9, 9);
+PrintArray (arr);
+SumPosNeg(arr);
+
+int[] MakeArray(int length, int min, int max)
+{
+    int[] arr = new int[length];
+    for(int i = 0; i < length; i++)
+    {
+        arr[i] = new Random().Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    int len = array.Length;
+    for (int i = 0; i < len; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+void SumPosNeg(int[] arr)
+{
+    int pos, neg;
+    pos = neg = 0;
+
+    for (int i = 0; i < arr.Length; ++i)
+    {
+        if(arr[i] >= 0)
+            pos += arr[i];
+        else
+            neg += arr[i];
+    }
+    Console.WriteLine(pos + " " + neg);
+}
+
