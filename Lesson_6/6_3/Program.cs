f1644f36f99cb9369d0cc08f5066401bcca95173
Ivan <vanuk@bk.ru>
2023-03-24 15:59:45 +0800

    6_3

---
+++ Lesson_6/6_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1
+
+int num = GetUserNumber("number");
+PrintFibonacci(num);
+
+int GetUserNumber(string name)
+{
+    int number = 0;
+    Console.Write($"Please enter {name}: ");
+    number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+void PrintFibonacci(int number)
+{
+    int a = 0;
+    int b = 1;
+    Console.Write($"{a}, {b}");
+    for (int i = 2; i < number; i++)
+    {
+        Console.Write($", {a + b}");
+        (a,b) = (b,a+b);
+    }
+}
+
