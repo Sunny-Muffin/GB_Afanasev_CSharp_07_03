74ea9fc500f51bfac5f052d4747b93a37af3ae1b
Ivan <vanuk@bk.ru>
2023-03-24 15:38:03 +0800

    6_1

---
+++ Lesson_6/6_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+int side1 = GetUserNumber("1st side");
+int side2 = GetUserNumber("2nd side");
+int side3 = GetUserNumber("3rd side");
+
+IsTriangle(side1, side2, side3);
+
+int GetUserNumber(string name)
+{
+    int number = 0;
+    Console.Write($"Please enter {name}: ");
+    number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+void IsTriangle (int a, int b, int c)
+{
+    if (a+b>c && a+c>b && c+b>a)
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+}
