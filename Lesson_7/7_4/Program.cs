18589b1da6db416b92ff2633528e676da772f03f
Ivan <vanuk@bk.ru>
2023-03-30 19:41:10 +0600

    7_4

---
+++ Lesson_7/7_4/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 4: Задайте двумерный массив. Введите элемент, и найдите первое его вхождение, 
+// выведите позиции по горизонтали и вертикали, или напишите, что такого элемента нет
+
+int arrRows = 5; 
+int arrColumns = 5; 
+int arrMin = 0; 
+int arrMax = 9; 
+
+
+int[,] arr = Make2DArray(arrRows, arrColumns, arrMin, arrMax);
+Print2DArray(arr);
+
+int searchElement = GetUserNumber("element, that you want to find");
+Console.WriteLine(FindElement(arr, searchElement));
+
+string FindElement(int[,] array, int element)
+{
+    int rows = array.GetUpperBound(0) + 1;
+    int columns = array.Length / rows;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (array[i,j] == element)
+            {
+                return($"First element {element} is in position ({i+1}, {j+1})");
+            }
+        }
+    }
+    return($"Your element {element} does not exist");
+}
+
+int GetUserNumber(string name)
+{
+    int number = 0;
+    Console.Write($"Please enter {name}: ");
+    number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int[,] Make2DArray(int rows, int columns, int min, int max)
+{
+    int[,] arr = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            arr[i,j] = new Random().Next(min, max + 1);
+        }
+    }
+
+    return arr;
+}
+
+void Print2DArray(int[,] array)
+{
+    int rows = array.GetUpperBound(0) + 1;
+    int columns = array.Length / rows;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
