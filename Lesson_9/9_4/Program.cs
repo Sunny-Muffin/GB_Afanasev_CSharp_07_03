9b8fa4583022ac1f4a772c4de8103605d911c622
Ivan Afanasev <vanuk@bk.ru>
2023-04-04 13:08:03 +0500

    9_4

---
+++ Lesson_9/9_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии
+
+int num = GetUserNumber("number");
+int degree = GetUserNumber("power");
+Console.WriteLine(ToThePower (num, degree));
+
+int GetUserNumber(string name)
+{
+    int number = 0;
+    Console.Write($"Please enter {name}: ");
+    number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int ToThePower (int number, int power)
+{
+    if (power == 1)
+    {
+        return number;
+    }
+    return ToThePower (number, power - 1) * number;
+}
